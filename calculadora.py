8683a9472a5bb24d907fd469c08136e424a77d7c
SamirHabibeh <sa.habibeh@duocuc.cl>
2026-07-01 14:24:09 -0400

    adicion de opc 5(salida)
    salida y despedida en opc 5 y cambios de if a elif.

--- calculadora.py
+++ calculadora.py
@@ -11,22 +11,26 @@ while True:
     print("1) Sumar");
     print("2) Restar");      
     print("3) Multiplicar");       
-    print("4- Dividir "); 
+    print("4) Dividir "); 
+    print("5) Salir")
 
     opc = int(input("Elige una opcion: "));     
 
     if (opc == 1):
         print(f"La suma es {num1+num2}");
         break;
-    if (opc == 2):
+    elif (opc == 2):
         print(f"La resta es {num1-num2}");
         break;
-    if (opc == 3):
+    elif (opc == 3):
         print(f"La multiplicacion es {num1*num2}");
         break;
-    if (opc == 4):
+    elif (opc == 4):
         print(f"La division es {num1/num2}");
         break;
+    elif(opc == 5):
+        print("Hasta pronto!.")
+        break
     else:
         print("Opcion no valida");
         break;
